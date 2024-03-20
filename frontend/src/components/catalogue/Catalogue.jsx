e19534259ac8c534c7dbb0dbbe11ce94ce3096d4
Bedknobs <noeliareysilva@gmail.com>
2024-03-20 10:10:51 +0100

    Añadido banner de catálogo

--- frontend/src/components/catalogue/Catalogue.jsx
+++ frontend/src/components/catalogue/Catalogue.jsx
@@ -4,7 +4,11 @@ function Catalogue() {
   return (
     <div className="catalogueContainer">
       <section>
-        <div className="banner"><h2>¡Empieza tu transformación hoy mismo!</h2></div>
+        <div className="banner">
+          <div className="bannerBg">
+            <h2>¡Empieza tu transformación hoy mismo!</h2>
+          </div>
+        </div>
         <div className="products"></div>
       </section>
     </div>
