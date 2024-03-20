e5bf9688c91c7d817fb8bba9b4a7601aa1c8af3c
Emii <151835514+Emilly3008@users.noreply.github.com>
2024-03-20 09:55:35 +0100

    catalogue

--- frontend/src/components/catalogue/Catalogue.jsx
+++ frontend/src/components/catalogue/Catalogue.jsx
@@ -1,9 +1,14 @@
-import "./Catalogue.css"
+import "./Catalogue.css";
 
 function Catalogue() {
   return (
-    <div>Catalogue</div>
-  )
+    <div className="catalogueContainer">
+      <section>
+        <div className="banner"><h2>¡Empieza tu transformación hoy mismo!</h2></div>
+        <div className="products"></div>
+      </section>
+    </div>
+  );
 }
 
-export default Catalogue
+export default Catalogue;
