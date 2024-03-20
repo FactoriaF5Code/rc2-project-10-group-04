4b8ee3dcb7366cd318ec5fcedd51380df8d02928
Bedknobs <noeliareysilva@gmail.com>
2024-03-20 12:54:45 +0100

    Añadido modelo de producto

--- backend/src/main/java/org/factoriaf5/backend/persistence/Product.java
+++ backend/src/main/java/org/factoriaf5/backend/persistence/Product.java
@@ -1,12 +1,60 @@
 package org.factoriaf5.backend.persistence;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
+@Table(name = "products")
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
     private String name;
     private String description;
     private double price;
-    private String img ;
-    
+    private String img;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
 }
