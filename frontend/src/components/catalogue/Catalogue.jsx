4e4d5f8e98a4cd560579f1225421e90a670e8d6e
chanita23 <sanamapeca@gmail.com>
2024-03-21 12:34:11 +0100

    conexion backend con frontend

--- frontend/src/components/catalogue/Catalogue.jsx
+++ frontend/src/components/catalogue/Catalogue.jsx
@@ -1,6 +1,12 @@
 import "./Catalogue.css";
+import { useFetch } from "./useFetch";
+import { useState } from "react";
 
 function Catalogue() {
+const{data} = useFetch("http://localhost:8080/product")
+const filteredData=data?.filter((product)=>product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+const [searchTerm,setSearchTerm]= useState("")
+
   return (
     <div className="catalogueContainer">
       <section>
@@ -9,7 +15,23 @@ function Catalogue() {
             <h2>¡Empieza tu transformación hoy mismo!</h2>
           </div>
         </div>
-        <div className="products"></div>
+        <div className="products">
+        {filteredData?.map((product) => (
+          <div key={product.id}>
+            <section className="container">
+              <section>
+                <img
+                  src={product.img}
+                  alt={product.name}
+                  
+                ></img>
+              </section>
+              
+              </section>
+            
+          </div>
+        ))}
+        </div>
       </section>
     </div>
   );
