0bfc6d384dfd7b19808fa8c70d05992ef3435f72
Bedknobs <noeliareysilva@gmail.com>
2024-03-22 12:26:57 +0100

    Arreglado el cors

--- frontend/src/pages/detailsProduct/DetailsProduct.jsx
+++ frontend/src/pages/detailsProduct/DetailsProduct.jsx
@@ -1,42 +1,46 @@
-import "./DetailsProduct.css"
+import "./DetailsProduct.css";
 import { useFetch } from "../../components/catalogue/useFetch";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useParams } from "react-router-dom";
 
-
-
 function DetailsProduct() {
-  const productId = useParams();
+  const { productId } = useParams(); // Obtén el parámetro 'productId' de la URL
   const { data } = useFetch(`http://localhost:8080/product/${productId}`);
   const [searchTerm, setSearchTerm] = useState("");
-  const filteredData = data?.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const [product, setProduct] = useState(null);
+
+  // Utiliza useEffect para actualizar el producto cuando cambia el ID
+  useEffect(() => {
+    setProduct(data);
+  }, [data]);
+
+  // Asegúrate de que la data del producto esté disponible antes de renderizar
+  if (!product) return null;
 
   return (
-    <div className="containerBg"><div className="bodyContainer">
-      <div className="searchContainer">
-        <input
-          className="searchInput"
-          type="search"
-          placeholder="Buscar..."
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <span className="searchIcon">
-          <ShoppingCartOutlinedIcon />
-        </span>
-      </div>
-      <div className="catalogueContainer">
-        <section className="bannerContainer">
-          <div className="banner">
-            <div className="bannerBg">
-              <h2>¡Empieza tu transformación hoy mismo!</h2>
+    <div className="containerBg">
+      <div className="bodyContainer">
+        <div className="searchContainer">
+          <input
+            className="searchInput"
+            type="search"
+            placeholder="Buscar..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <span className="searchIcon">
+            <ShoppingCartOutlinedIcon />
+          </span>
+        </div>
+        <div className="catalogueContainer">
+          <section className="bannerContainer">
+            <div className="banner">
+              <div className="bannerBg">
+                <h2>¡Empieza tu transformación hoy mismo!</h2>
+              </div>
             </div>
-          </div>
 
-          <div className="products">
-            {filteredData?.map((product) => (
+            <div className="products">
               <div key={product.id}>
                 <section className="containerImg">
                   <section>
@@ -44,15 +48,12 @@ function DetailsProduct() {
                   </section>
                 </section>
               </div>
-            ))}
-          </div>
-
-        </section>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default DetailsProduct
+export default DetailsProduct;
